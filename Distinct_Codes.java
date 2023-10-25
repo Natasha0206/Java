27146bfbb88cadb215fe4a2c59466380b133ecbe
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-10-26 00:43:33 +0530

    Add files via upload

---
+++ Distinct_Codes.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    String str=sc.next();
+		    HashSet<String> set =new HashSet<>();
+		    int count=0;
+		    for(int i=0;i<str.length()-1;i++)
+		    {
+		        
+		        String st=""+str.charAt(i)+str.charAt(i+1);
+		        if(set.contains(st))
+		        {
+		            continue;
+		        }
+		        else
+		        {
+		            set.add(st);
+		            count++;
+		        }
+		    }
+		    
+		    System.out.println(count);
+		    
+		}
+	}
+}
